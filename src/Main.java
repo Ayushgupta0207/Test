4ccb46e4ce375608b343b75ee14002e3266a7ecd
ANSH SAXENA <60190254+FlowerbeanAnsh@users.noreply.github.com>
2021-10-01 11:52:43 +0530

    Update Main.java
    Kindly accept the changes.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("ayush");
-    }
+import java.util.*;
 
+public class Main
+{  // sum of n natural numbers
+
+	public static void main(String[] args) {
+	   Scanner kb = new Scanner(System.in);
+	   int n = kb.nextInt();
+	   int sum=0;
+	   for(int i=1;i<=n;i++){
+	       sum = sum + i;
+	   }
+	   System.out.println(sum);
+	   
+	}
 }
